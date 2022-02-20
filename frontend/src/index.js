55d716ab31c9ea8393a88dab591a6222ecae1460
kotaro1574 <shake1574@gmail.com>
2022-02-21 06:50:13 +0900

    create: data取得

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ApolloClient, ApolloProvider, gql, InMemoryCache, useQuery} from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: 'http://localhost:4000',
+    cache: new InMemoryCache()
+});
+
+const BOOKS = gql`
+    query Books {
+        books {
+            title
+            author
+        }
+    }
+`;
+
+function Books() {
+    const {loading, error, data} = useQuery(BOOKS);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+    return data.books.map(({title, author}) => (
+        <div key={title}>
+            <p>
+                {title} by {author}
+            </p>
+        </div>
+    ));
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ApolloProvider client={client}>
+            <App/>
+            <Books/>
+        </ApolloProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
